d2295cd41e1b330badcc0e8ace6c5801ad7cb906
mehanig <mehanig@gmail.com>
2015-01-14 23:08:23 +0400

    Video starts

--- stepicstudio/FileSystemOperations/action.py
+++ stepicstudio/FileSystemOperations/action.py
@@ -1,9 +1,15 @@
 import os
+import signal
 import shutil
 import time
 import xml.etree.ElementTree as ET
+import xml.parsers.expat
 from stepicstudio.utils.extra import translate_non_alphanumerics
+from STEPIC_STUDIO.settings import ADOBE_LIVE_EXE_PATH
+import subprocess
+import codecs
 
+GlobalProcess = None
 
 def substep_server_path(**kwargs):
     folder = kwargs["folder_path"]
@@ -35,9 +41,33 @@ def add_file_to_test(**kwargs):
     if not os.path.isdir(folder_p):
         os.makedirs(folder_p)
 
-    time.sleep(2)
     return True
 
+
+#TODO CHANGE THIS SHIT! ASAP! HATE WINDOWS!
+def run_adobe_live():
+    # GlobalProcess = subprocess.Popen(ADOBE_LIVE_EXE_PATH, stdout=subprocess.PIPE)
+    # stdout, stderr = GlobalProcess.communicate()
+    # print(stdout)
+    #
+    # if not GlobalProcess.returncode:
+    #     return True
+    # else:
+    #     return False
+    p = [r"D:\stream_profile\stepic_run_camera.bat"]
+    global GlobalProcess
+    p = [r"C:\Program Files (x86)\Adobe\Flash Media Live Encoder 3.2\FMLECmd.exe",
+         "/p", r"C:\StepicServer\static\video\xml_settings.xml"]
+    GlobalProcess = subprocess.Popen(p, shell=False)
+    print("From Run", GlobalProcess.pid)
+    return True
+
+
+def stop_adobe_live():
+    p = [r"C:\Program Files (x86)\Adobe\Flash Media Live Encoder 3.2\FMLECmd.exe",
+         "/s", r"C:\StepicServer\static\video\xml_settings.xml"]
+    print("From Stop", GlobalProcess.pid)
+    os.kill(GlobalProcess.pid, signal.SIGTERM)
 
 def delete_substep_on_disc(**kwargs):
     folder = kwargs["folder_path"]
@@ -60,7 +90,6 @@ def delete_step_on_disc(**kwargs):
     f_c_lesson = f_course + "/" + translate_non_alphanumerics(data["Lesson"].name)
     f_c_l_step = f_c_lesson + "/" + translate_non_alphanumerics(data["Step"].name)
     return delete_files_on_server(f_c_l_step)
-
 
 
 def files_txt_update(**kwargs):
@@ -102,11 +131,8 @@ def delete_files_on_server(path):
 def generate_xml(XMLpath, write_to_path, file_name):
     tree = ET.parse(XMLpath)
     root = tree.getroot()
-    for elem in root.iter('directory'):
-        print(elem.text)
-        elem.text = write_to_path
     for elem in root.iter('path'):
-        print(elem.text)
-        elem.text = file_name
-    tree.write(XMLpath)
-    pass
+        elem.text = write_to_path.replace("/", "\\") + "\\" + file_name.replace("/", "\\")
+    #     # <?xml version="1.0" encoding="UTF-16"?>
+    tree.write(XMLpath, encoding="UTF-16", short_empty_elements=False)
+    # ElementTree.tostring(tree, encoding='utf-16')
