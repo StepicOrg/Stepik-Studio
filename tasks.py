2cd57beb92b40afff00ce26d530eec0155f81c44
mehanig <mehanig@gmail.com>
2015-05-08 03:34:44 +0300

    first tests added

---
+++ tasks.py
@@ -0,0 +1,14 @@
+from invoke import run, task
+
+@task
+def test(cov=True, verbose=False):
+    """
+    Runs all tests in the 'tests/' directory
+    """
+    cmd = 'py.test tests'
+    if verbose:
+        cmd += ' -v'
+    if cov:
+        cmd += ' --cov-report term-missing --cov-config .coveragerc --cov stepic_studio'
+
+    run(cmd, pty=True)
