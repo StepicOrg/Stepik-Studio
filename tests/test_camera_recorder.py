4c59fa2c9df171219b15732e2e5156df120c9e0c
user <i.pospelov@g.nsu.ru>
2018-09-25 19:56:44 +0700

    experimental ffmpy video recorder

---
+++ tests/test_camera_recorder.py
@@ -0,0 +1,17 @@
+import time
+from django.test import TestCase
+from stepicstudio.VideoRecorder.camera_recorder import CameraRecorder
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'STEPIC_STUDIO.settings'
+
+
+# functional tests
+
+class TestCameraRecorder(TestCase):
+    def test_should_record_from_camera(self):
+        output_file = 'C:\\Development\\Tests\\test_record.TS'
+        cam_recorder = CameraRecorder(output_file)
+        cam_recorder.start_recording()
+        time.sleep(10)
+        cam_recorder.stop_recording()
