4d0828996bab286fd0c62a5dac3f27e15f4d06de
pospelov14203 <freebush.john@gmail.com>
2018-10-02 22:29:09 +0700

    WIP: tablet recording refactoring

--- stepicstudio/ssh_connections/tablet_client.py
+++ stepicstudio/ssh_connections/tablet_client.py
@@ -10,22 +10,48 @@ class TabletClient(object):
         self.__connect()
 
     def execute_remote(self, command: str) -> str:
+        if not self.__is_alive():
+            self.__connect()
+
         _, stdout, _ = self.ssh.exec_command(command)
         exit_status = stdout.channel.recv_exit_status()
+
         if exit_status > 0:
+            self.__logger.error('Can\'t execute %s, exit status: %s', command, exit_status)
             raise RuntimeError('Cannot execute command {0} [returned code: {1}]'.format(command, exit_status))
+
         return stdout.readlines()
 
-    def __connect(self):
-        self.ssh = paramiko.SSHClient()
-        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.ssh.connect(settings.PROFESSOR_IP,
-                         username=settings.UBUNTU_USERNAME,
-                         password=settings.UBUNTU_PASSWORD)
+    def check_and_create_folder(self, path):
+        if not self.__is_alive():
+            self.__connect()
 
-        transport = paramiko.Transport((settings.PROFESSOR_IP, 22))
-        transport.connect(username=settings.UBUNTU_USERNAME, password=settings.UBUNTU_PASSWORD)
-        self.sftp = paramiko.SFTPClient.from_transport(transport)
+        splitted_path = path.split('/')
+        splitted_path = list(filter(None, splitted_path))
+        temp_path = '/'
+
+        for subfolder in splitted_path:
+            temp_path += subfolder
+            try:
+                self.sftp.stat(temp_path)
+            except IOError:
+                self.__logger.info('Will create folder %s at tablet', temp_path)
+                self.sftp.mkdir(temp_path)
+
+    def __connect(self, attempts=1):
+        for _ in range(1, attempts):
+            self.ssh = paramiko.SSHClient()
+            self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.ssh.connect(settings.PROFESSOR_IP,
+                             username=settings.UBUNTU_USERNAME,
+                             password=settings.UBUNTU_PASSWORD)
+
+            transport = paramiko.Transport((settings.PROFESSOR_IP, 22))
+            transport.connect(username=settings.UBUNTU_USERNAME, password=settings.UBUNTU_PASSWORD)
+            self.sftp = paramiko.SFTPClient.from_transport(transport)
+
+        if not self.__is_alive():
+            raise Exception('Invalid ssh connection')
 
     def __is_alive(self):
         return self.ssh.get_transport().is_alive()
--- stepicstudio/video_recorders/tablet_recorder.py
+++ stepicstudio/video_recorders/tablet_recorder.py
@@ -1,4 +1,8 @@
+import logging
+
 from singleton_decorator import singleton
+
+from stepicstudio import const
 from stepicstudio.operationsstatuses.operation_result import InternalOperationResult
 from stepicstudio.operationsstatuses.statuses import ExecutionStatus
 from stepicstudio.ssh_connections.tablet_client import TabletClient
@@ -10,21 +14,39 @@ from django.conf import settings
 class TabletScreenRecorder(PostprocessableRecorder):
     def __init__(self):
         super().__init__()
-        self.__tablet_client = TabletClient()
         self.__command = settings.FFMPEG_TABLET_CMD
+        self.__logger = logging.getLogger('stepic_studio.video_recorders.tablet_recorder')
         self.__last_processed_path = None
         self.__last_processed_file = None
         self._load_postprocessing_pipe(settings.TABLET_POSTPROCESSING_PIPE)
 
+        try:
+            self.__tablet_client = TabletClient()
+        except Exception as e:
+            self.__logger.error('Can\'t initialize tablet client: %s', e)
+            raise e
+
     def start_recording(self, path: str, filename: str) -> InternalOperationResult:
         if self.is_active():
-            self.__logger.error('Can\'t start FFMPEG for file %s: screen is acctually recording ', path + '/' + filename)
+            self.__logger.error('Can\'t start FFMPEG for file %s: screen is acctually recording ',
+                                path + '/' + filename)
             return InternalOperationResult(ExecutionStatus.FATAL_ERROR, 'Tablet is actually recording')
 
+        self.__tablet_client.check_and_create_folder(path)
+
+        command = settings.FFMPEG_TABLET_CMD + path + filename + const.SUBSTEP_SCREEN + ' 2< /dev/null &'
         # local_command = self.__command + os.path.join(path, filename)
 
     def stop_recording(self) -> InternalOperationResult:
-        pass
+        command = 'pkill -f ffmpeg'
+
+        try:
+            self.__tablet_client.execute_remote(command)
+        except Exception as e:
+            self.__logger.error('Problems while stop screen recording: %s', e)
+            return InternalOperationResult(ExecutionStatus.FATAL_ERROR)
+
+        return InternalOperationResult(ExecutionStatus.SUCCESS)
 
     def is_active(self) -> bool:
         pass
