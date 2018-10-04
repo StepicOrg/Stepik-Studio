6406940a4778f7f3321d7cb7df028ad0efc541a4
user <i.pospelov@g.nsu.ru>
2018-10-04 21:37:45 +0700

    Scheduled synchronization

--- stepicstudio/scheduling/task_manager.py
+++ stepicstudio/scheduling/task_manager.py
@@ -1,9 +1,11 @@
 import logging
 from multiprocessing import Queue
 
-from django.conf import settings
+from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
+from django.conf import settings
 from singleton_decorator import singleton
+from tzlocal import get_localzone
 
 logging.getLogger('apscheduler').setLevel(logging.WARNING)
 
@@ -11,7 +13,7 @@ logging.getLogger('apscheduler').setLevel(logging.WARNING)
 @singleton
 class TaskManager(object):
     def __init__(self):
-        self.scheduler = BackgroundScheduler({'apscheduler.timezone': 'UTC'})
+        self.scheduler = BackgroundScheduler(timezone=get_localzone())
         self.scheduler.start()
         self.queue = Queue()
         self.__logger = logging.getLogger('stepicstudio.scheduling.tesk_manager')
@@ -19,13 +21,18 @@ class TaskManager(object):
         #  sched.add_job(my_job, trigger='cron', hour='22', minute='30')
 
     def run_while_idle_once_time(self, job: callable):
-        self.__logger.info('New task for once time idle execution! Current queue size: %s tasks.', self.queue.qsize())
+        self.__logger.info('New task for once time idle execution! Current queue size: %s tasks. Current time: %s; Execution at %s:00.',
+                           self.queue.qsize(),
+                           datetime.now(self.scheduler.timezone),
+                           settings.BACKGROUND_TASKS_START_HOUR)
         self.queue.put(job)
 
     def repeat_while_idle(self, job: callable):
         self.scheduler.add_job(job, 'cron', second='*')
 
     def __execute_jobs(self):
+        self.__logger.info('Executing scheduled tasks. Size of queue before executing: %s.',
+                           self.queue.qsize())
         try:
             while not self.queue.empty():
                 task = self.queue.get()
