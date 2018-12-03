ae1f6df832306abb431b2a9da32c69e515194516
pospelov14203 <i.pospelov@g.nsu.ru>
2018-12-03 17:34:13 +0700

    WIP: premiere scripting

--- stepicstudio/postprocessing/exporting/adobe_scripts/create_project.jsx
+++ stepicstudio/postprocessing/exporting/adobe_scripts/create_project.jsx
@@ -1,20 +1,23 @@
-﻿//Based on samples of using ExtendScript and docs: 
+﻿#target premierepro
+
+//Based on samples of using ExtendScript and docs:
 //https://github.com/Adobe-CEP/Samples/blob/master/PProPanel/jsx/PPRO/Premiere.jsx
 //https://media.readthedocs.org/pdf/premiere-scripting-guide/latest/premiere-scripting-guide.pdf
 
-//Creates sequence
-//params:
-//name - name of sequence as string
-//preset - path to .sqpreset file as string
+
+/*Creates sequence
+params:
+name - name of sequence as string
+preset - path to .sqpreset file as string*/
 function createSequence(name, preset) {
     app.enableQE();
     qe.project.newSequence(name, preset);
 }
 
-//Appends to sequence
-//params:
-//item - projectItem to append to sequence
-//targetVTrackNumber - number of track to append
+/*Appends item to sequence
+params:
+item - projectItem to append to sequence
+targetVTrackNumber - number of track to append*/
 function appendItemToSequence(item, targetVTrackNumber) {
     var seq = app.project.activeSequence;
     if (seq){
@@ -26,7 +29,7 @@ function appendItemToSequence(item, targetVTrackNumber) {
             var targetVTrack = seq.videoTracks[targetVTrackNumber];
             if (targetVTrack){
                 // If there are already clips in this track,
-                // append this one to the end. Otherwise, 
+                // append this one to the end. Otherwise,
                 // insert at start time.
 
                 if (targetVTrack.clips.numItems > 0){
@@ -48,29 +51,29 @@ function appendItemToSequence(item, targetVTrackNumber) {
     }
 }
 
+/*Appends list of items to sequence
+params:
+path - base path to directory with media items
+files - array of file names of media items
+trackNumber - track number of sequence*/
 function appendToSequence(path, files, trackNumber) {
     fullPaths = files.map(function(fileName) {
         return path + fileName;
     });
-
     app.project.importFiles(fullPaths);
-    
     for(var i = 0; i < app.project.rootItem.children.numItems; i++) {
         if(files.indexOf (app.project.rootItem.children[i].name) > -1) {
             appendItemToSequence(app.project.rootItem.children[i], trackNumber);
-            k++;
         }
     }
 }
 
-var basePath = "D:\\STEPIKSTUDIO\\TESTER\\test_course\\Lesson_1123123\\Step123123123\\"
-var professorVideos = ["Step2from367_Professor.mp4",  "Step3from367_Professor.mp4"]; 
-var screenVideos = ["Step2from367_Screen.mp4", "Step3from367_Screen.mp4"]
-var preset = "C:\\Development\\AdobeScripts\\PProPanel.sqpreset";
+var basePath = "D:\\STEPIKSTUDIO\\TESTER\\test_course\\Lesson_1123123\\Step123123123\\";
+var professorVideos = ["Step2from367_Professor.mp4",  "Step3from367_Professor.mp4"];
+var screenVideos = ["Step2from367_Screen.mp4", "Step3from367_Screen.mp4"];
+var preset = "C:\\Development\\Stepik-Studio\\stepicstudio\\postprocessing\\exporting\\adobe_templates\\ppro.sqpreset";
 
 createSequence("testSequence", preset);
-    
-//Imports files:
 appendToSequence(basePath, professorVideos, 1);
 appendToSequence(basePath, screenVideos, 0);
 
